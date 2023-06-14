7cf52ba3dd7f3f4c800240b2a050239e8d39cf93
liebespaar93 <liebespaar93@gmail.com>
2023-06-15 03:04:30 +0900

    ready to parse

---
+++ src/UserControl.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iomanip>
+
+#include "UserControl.hpp"
+
+
+UserControl::UserControl()
+{
+	this->_fd_map = new std::map<const int, User *>;
+	this->_name_map = new std::map<const std::string, int>;
+	this->_invite_map = new std::map<std::string, Channel *>;
+	std::cout << std::setw(15) << "[UserControl] " << "create!!" << std::endl;
+}
+
+UserControl::UserControl(const UserControl& ref)
+{
+	*this=ref;
+	std::cout << std::setw(15) << "[UserControl] " << "copy!!" << std::endl;
+}
+
+UserControl::~UserControl()
+{
+	std::cout << std::setw(15) << "[UserControl] " << "delete!!" << std::endl;
+}
+
+UserControl&	UserControl::operator=(const UserControl& ref)
+{
+	this->_fd_map = ref._fd_map;
+	this->_name_map = ref._name_map;
+	this->_invite_map = ref._invite_map;
+	std::cout << std::setw(15) << "[UserControl] " << "operator=!!" << std::endl;
+	return (*this);
+}
+
+void	UserControl::ft_append_user(User *user)
+{
+	this->_fd_map->insert( std::pair<const int, User *>(user->ft_get_fd(), user ));
+	this->_name_map->insert( std::pair<std::string, const int>( user->ft_get_user_name(), user->ft_get_fd() ));
+}
+
+User   *UserControl::ft_get_user(const int &fd)
+{
+	std::map<const int, User *>::iterator it = this->_fd_map->find(fd);
+	if (it == this->_fd_map->end())
+		return (NULL);
+	return ((*it).second);
+}
+
+User	*UserControl::ft_get_user(const std::string &user_name)
+{
+	std::map<const std::string, int>::iterator it = this->_name_map->find(user_name);
+	if (it == this->_name_map->end())
+		return (NULL);
+	return (this->ft_get_user((*it).second));
+}
+
+
+
+void UserControl::ft_invite_channel(std::string user_name, Channel *user_channel)
+{
+	User *invite_user = this->ft_get_user(user_name);
+	this->_invite_map->insert(  std::pair<std::string, Channel *>(user_name, user_channel));
+}
+
+
+
+// bool	Channel::ft_channel_invite_refuse(User *user)
+// {
+// 	if (this->_invite_list.at(user->ft_get_user_name()));
+// 	{
+// 		this->_invite_list.erase(user->ft_get_user_name());
+// 	}
+// }
+// 	if (this->_invite_list.at(user->ft_get_user_name()));
+// 	{
+// 		this->_invite_list.erase(user->ft_get_user_name());
+// 	}
+    // ft_channel_invite_accept() = leave & join
--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,18 @@ int main(int argc, char const *argv[])
 	std::string port_str, password_str;
 
 	if (argc != 3)
-	{
-		Logger("argc != 3 \n[TIP] ./server [port] [password]").ft_error();
-		return (1);
-	}
+		return (Logger("argc != 3 \n[TIP] ./server [port] [password]").ft_error(), 1);
 	port_str = argv[1];
 	password_str = argv[2];
 	if (port_str.find_first_not_of("0123456789") != std::string::npos || std::atoll(port_str.c_str()) > 65535)
-	{
-		Logger("port is not digit").ft_error();
-		return (2);
-	}
-
+		return (Logger("port is not digit").ft_error(), 2);
 	try
 	{
 		server = new Server(port_str, password_str);
 		server->ft_server_on();
 		delete server;
 	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	catch (const std::exception &e){ std::cerr << e.what() << '\n';}
 	system("leaks server");
 	return 0;
 }
