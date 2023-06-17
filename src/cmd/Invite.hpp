085c90fa692374e810893da8d23d805c0b4fd22b
Gyeong Ju Kim <gyeongjukim@Gyeongs-MacBook-Air.local>
2023-06-18 00:07:36 +0900

    add some comments

--- src/cmd/Invite.hpp
+++ src/cmd/Invite.hpp
@@ -1,23 +1,23 @@
 #ifndef INVITE_HPP
-# define INVITE_HPP
-
+#define INVITE_HPP
 
 #include "Cmd.hpp"
 
-
-class Invite: public Cmd
+class Invite : public Cmd
 {
 private:
 	/* data */
-	Invite(const Invite& ref);
+	Invite(const Invite &ref);
 
-	Invite&	operator=(const Invite& ref);
+	Invite &operator=(const Invite &ref);
 
 public:
 	Invite() { this->_cmd = "INVITE"; };
 	~Invite(){};
 
-	
+	// irssi로 여러 클라이언트를 돌리면서 /mode로 invtite only 설정을 해도 join이 되어서 테스트하기 굉장히 까다로움
+	// lime으로 모드 설정과 invtite 테스트 예정
+	// irssi로 했을시 초대받았다라는 메시지만 받고 채널 입장은 하지 않음
 };
 
 #endif
