31bee17a2db667031a147242a45c73538cf2a89b
Gyeong Ju Kim <gyeongjukim@Gyeongs-MacBook-Air.local>
2023-06-20 23:10:27 +0900

    Mode & Topic done, but need to double-check

--- src/cmd/Invite.hpp
+++ src/cmd/Invite.hpp
@@ -15,9 +15,8 @@ public:
 	Invite() { this->_cmd = "INVITE"; };
 	~Invite(){};
 
-	// irssi로 여러 클라이언트를 돌리면서 /mode로 invtite only 설정을 해도 join이 되어서 테스트하기 굉장히 까다로움
-	// lime으로 모드 설정과 invtite 테스트 예정
 	// irssi로 했을시 초대받았다라는 메시지만 받고 채널 입장은 하지 않음
+	// k 설정되어도 초대받으면 조인가능
 };
 
 #endif
--- src/cmd/Topic.hpp
+++ src/cmd/Topic.hpp
@@ -14,16 +14,49 @@ public:
 	~Topic();
 
 	Topic &operator=(const Topic &ref);
-	// /topic은 request랑 response가 없다...?
-	// /topic 정하기 전에 '/topic'을 하면 'No topic set for #room_name'
-	// '/topic abc def ghi'를 하면
-	// Request: TOPIC #test02 :abc def ghi
-	// Response: :gyeongjukim!~gyeongjuk@2748-3f2-dd20-23f5-64d.50.120.ip TOPIC #test02 :abc def ghi
-	// gyeongjukim changed the topic of #test02 to: abc def ghi
-	// '/topic' 하면 아래 메시지가 채팅방에 나오고 request 요청은 없다.
-	// 23:55 -!- Topic for #test02: abc def ghi
-	// 23:55 -!- Topic set by gyeongjukim
-	//           [~gyeongjuk@2748-3f2-dd20-23f5-64d.50.120.ip] [Sat Jun 17 23:54:21 2023]
+
+	void ft_recv(std::vector<std::string> msg)
+	{
+		if (msg.size() == 2)
+		{
+			// Response: :*.freenode.net 331 gye #test00 :No topic is set.
+			if (this->_user->ft_get_channel(msg[1])->ft_get_topic() == "")
+			{
+				this->ft_set_client("331");
+				this->_send_msg = RPL_NOTOPIC(this->_client, msg[1]);
+				this->ft_send();
+				return;
+			}
+			else
+			{
+				this->_send_msg = this->_user->ft_get_channel(msg[1])->ft_get_topic();
+				this->ft_send();
+				return;
+			}
+		}
+		else if (msg.size() > 2)
+		{
+			if (!this->_user->ft_get_channel(msg[1])->ft_privilege_has_user(this->_user->ft_get_user_name()))
+			{
+				// Response: :*.freenode.net 482 gyeongjukim #test00 :You do not have access to change the topic on this channel
+				// ERR_CHANOPRIVSNEEDED
+				this->ft_set_client("482");
+				this->_send_msg = ERR_CHANOPRIVSNEEDED(this->_client, msg[1]);
+				this->ft_send();
+				return;
+			}
+			else
+			{
+				// Response: :gye!~gyeokim@freenode-n68.49c.2i380a.IP TOPIC #test00 :this is a test
+				this->_user->ft_get_channel(msg[1])->ft_set_topic(msg[3]);
+				this->_send_msg = ":" + this->_user->ft_get_nick_name() + this->_user->ft_get_user_name() + "@" +
+								  this->_user->ft_get_IP() + " " + this->_cmd + " " + msg[1] + " :" +
+								  this->_user->ft_get_channel(msg[1])->ft_get_topic();
+				this->ft_send();
+				return;
+			}
+		}
+	}
 };
 
 #endif
